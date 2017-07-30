e2bbcc164c6133792c97753360e960f6ab0a5663
BotMaster3000 <jdgebauer@gmx.de>
2017-07-30 16:49:17 +0200

    Initial commit

---
+++ Cell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellSimulator
+{
+    public class Cell
+    {
+        Random rand = new Random();
+
+        public int id;
+        public int age = 0;
+
+        public int food = 100;
+        public int maxFood = 100;
+
+        public int energy = 100;
+        public int maxEnergy = 100;
+        
+        public Cell(int cellId)
+        {
+            id = cellId;
+        }
+
+        public void Eat()
+        {
+            bool didEat = false;
+            if(rand.Next(0, 101) > 75)
+            {
+                didEat = true;
+            }
+            if (didEat)
+            {
+                food += 10;
+            }
+            if(food > maxFood)
+            {
+                food = maxFood;
+            }
+        }
+        public bool Split()
+        {
+            bool didSplit = false;
+            if(rand.Next(0, 101) > 75)
+            {
+                didSplit = true;
+            }            
+            return didSplit;
+        }
+    }
+}
---
+++ CellOverseer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CellSimulator
+{
+    public class CellOverseer
+    {
+        public List<Cell> cells = new List<Cell>();
+        public int cellIdCounter = 0;
+
+        public CellOverseer(int startingCells)
+        {
+            for(int i = 0; i < startingCells; ++i)
+            {
+                cells.Add(new Cell(cellIdCounter));
+                ++cellIdCounter;
+            }
+        }
+        public void SimulateNext()
+        {
+            List<Cell> stillAliveCells = new List<Cell>();
+            foreach(Cell cell in cells)
+            {
+                if(cell.energy <= 0)
+                {
+                    continue;
+                }
+                if(cell.food > 80)
+                {
+
+                }
+                else
+                {
+                    cell.Eat();
+                }
+                stillAliveCells.Add(cell);
+            }
+        }
+    }
+}
