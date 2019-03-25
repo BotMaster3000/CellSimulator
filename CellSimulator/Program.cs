c0c30c08cf8dafb90ec3e68fbeb62e1f64c0f46a
BotMaster3000 <jdgebauer@gmx.de>
2019-03-25 19:33:15 +0100

    Changed File Versioning, Removed the old Cell.cs and Celloverseer.cs as well as all unneccessary lines from the Program.cs

--- CellSimulator/Program.cs
+++ CellSimulator/Program.cs
@@ -9,52 +9,10 @@ namespace CellSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             SimulationManager manager = new SimulationManager();
             manager.StartSimulation();
-            return;
-
-            bool showCellInfo = true;
-            CellOverseer overseer = new CellOverseer(1);
-            while (true)
-            {
-                if (showCellInfo)
-                {
-                    foreach (Cell cell in overseer.cells)
-                    {
-                        Console.WriteLine("ID:" + cell.id + "|AGE:" + cell.age + "|FOOD:" + cell.food + "|ENERGY:" + cell.energy + "|ACTION:" + cell.lastAction);
-                    }
-                }
-                Console.WriteLine("ALIVE:" + overseer.cells.Count());
-                string commandEntered = Console.ReadLine().ToLower();
-                if (!string.IsNullOrEmpty(commandEntered))
-                {
-                    switch (commandEntered)
-                    {
-                        case "save":
-                            overseer.SaveToFile();
-                            continue;
-                        case "load":
-                            string fileName = Console.ReadLine();
-                            if (string.IsNullOrEmpty(fileName))
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                overseer.LoadFromFile(fileName);
-                            }
-                            break;
-                        case "switchshowinfo":
-                            showCellInfo = !showCellInfo;
-                            break;
-                    }
-                }
-
-                overseer.SimulateNext();
-                Console.WriteLine();
-            }
         }
     }
 }
